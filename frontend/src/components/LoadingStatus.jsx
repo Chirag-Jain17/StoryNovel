05e6d3dc436695cd6136bad04b6560afd6321b5a
Chirag-Jain17 <jainchirag2005@gmail.com>
2025-12-23 19:55:56 +0530

    Created frontend, set up most componenets, and attached the routers

---
+++ frontend/src/components/LoadingStatus.jsx
@@ -0,0 +1,14 @@
+function LoadingStatus({theme}) {
+    return <div className="loading-container">
+        <h2>Generating your {theme} Story</h2>
+
+        <div className="loading-animation">
+            <div className="spinner"></div>
+        </div>
+        <p className="loading-info">
+            Please wait while the story is being generated
+        </p>
+    </div>
+}
+
+export default LoadingStatus
